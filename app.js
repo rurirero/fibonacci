cd80fe335b2d353251cfe27ba2b078255b1e44d2
rurirero <hikarunnba971@gmail.com>
2022-11-28 00:49:08 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 'use strict';
+/*
 function fib (n) {
     if (n === 0) {
         return 0;
@@ -11,3 +12,19 @@ const length = 40;
     for (let i = 0;i <= length; i++){
         console.log(fib(i))
     };
+*/
+const memo = new Map();
+memo.set(1,1);
+memo.set(0,0);
+function fib(n) {
+    if (memo.has(n)) {
+        return memo.get(n);
+    }
+    const valune = fib(n - 1)+fib(n - 2);
+    memo.set(n,valune);
+    return valune;
+};
+const length = 40;
+for (let i = 0; i <= length; i++) {
+    console.log(fib(i));
+};
